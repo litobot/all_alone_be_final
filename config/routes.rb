50253b987d8918f5427a32fd8d878a7938a0eb79
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-24 15:13:10 -0600

    Add tests for returning a merchant's invoices w/formatting

--- config/routes.rb
+++ config/routes.rb
@@ -28,13 +28,11 @@ Rails.application.routes.draw do
   get "/api/v1/items/:id/merchant", to: "api/v1/merchant_items#show"
 
   get "/api/v1/merchants/:merchant_id/customers", to: "api/v1/merchant_customers#index"
-
   get "/api/v1/merchants/:id/items", to: "api/v1/item_merchants#index"
-
+  get "/api/v1/merchants/:merchant_id/invoices/:id", to: "api/v1/invoices#show"
 
   get "/api/v1/merchants/:merchant_id/coupons", to: "api/v1/coupons#index"
   get "/api/v1/merchants/:merchant_id/coupons/:id", to: "api/v1/coupons#show"
   post "/api/v1/merchants/:merchant_id/coupons", to: "api/v1/coupons#create"
   patch "/api/v1/merchants/:merchant_id/coupons/:id", to: "api/v1/coupons#update"
-
 end
