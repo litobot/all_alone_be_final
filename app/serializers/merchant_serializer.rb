ddc4a5019fdbeaa2d2b18d8f21616fd2a159d562
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-09-24 18:38:45 -0600

    Show updated merchant serializer + AR queries functional

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -4,4 +4,6 @@ class MerchantSerializer
   attributes :name
 
   attribute :item_count, if: Proc.new {|merchants, params| params && params[:action] == "index" }
+  attribute :coupons_count, if: Proc.new {|merchants, params| params && params[:action] == "index" }
+  attribute :invoice_coupon_count, if: Proc.new {|merchants, params| params && params[:action] == "index" }
 end
